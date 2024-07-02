3d26997b41c1a7bcd558dcda02a04c3573a6c3fc
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-07-02 02:21:49 -0400

    fixes

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use(unknownEndpoint);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
