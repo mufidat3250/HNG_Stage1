af6e94648219ea3aa6007ad1b73ac345668aa835
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-07-02 02:08:10 -0400

    fixes

--- index.js
+++ index.js
@@ -9,19 +9,24 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/hello", async (request, response) => {
-  const { visitor_name } = request.query;
-  const ipAddress = request.ip.slice(7);
-  const {location, temperature} = await fetchweatherData()
-  response
-    .status(200)
-    .json({
-      client_IP: ipAddress,
-      visitor_name: visitor_name,
-      location,
-      greeting: `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celcius in ${location}`,
-    });
-});
+app.get("/api/hello", (req, response) => {
+    response.send(`hello world`)
+})
+
+
+// app.get("/api/hello", async (request, response) => {
+//   const { visitor_name } = request.query;
+//   const ipAddress = request.ip.slice(7);
+//   const {location, temperature} = await fetchweatherData()
+//   response
+//     .status(200)
+//     .json({
+//       client_IP: ipAddress,
+//       visitor_name: visitor_name,
+//       location,
+//       greeting: `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celcius in ${location}`,
+//     });
+// });
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
